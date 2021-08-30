fd8db47d2e9b57e96c1cb90ee5ef23f99dd83942
Kamlesh Kamble <kamlesh.kamble2.95@gmail.com>
2021-08-31 02:42:08 +0530

    created product components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
+import React from "react";
+import Products from "./components/Products";
+import data from "./data.json"
 
-
-function App() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    }
+  }
+  render(){
   return (
     <div className="grid-container">
       <header className="App-header">
         <a href="/">The Tee Store</a>
       </header>
       <main>
-        Product List
+        <div className="content">
+          <div className="main">
+              <Products products={this.state.products} />
+          </div>
+          <div className="sidebar">Cart Items</div>
+        </div>
       </main>
       <footer>
         All Rights Reserved 
       </footer>
     </div>
   );
+  }
 }
 
 export default App;
